13b9155fad7b31ea1d9c8c3c07b38b09a666b511
stng-ray <133681261+stng-ray@users.noreply.github.com>
2023-06-01 15:33:13 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,8 @@
+import requests
+url = "https://api.ravelry.com/projects/search.json?query=puppycat"
+
+response = requests.get(url)
+ravelry_things = response.json()
+ravelry_things
+
+print(ravelry_things)
