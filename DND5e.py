ff1c0d5002af4c03cf44a9f0f092f659b0256ce4
stng-ray <133681261+stng-ray@users.noreply.github.com>
2023-06-02 16:10:45 +0000

    marc helped us figure out how to print a random alignment!

--- DND5e.py
+++ DND5e.py
@@ -1,16 +1,27 @@
-import requests 
+import requests
 
-URL = "https://www.dnd5eapi.co/api/races"
+URL = "https://www.dnd5eapi.co/api/alignments"
 response = requests.get(URL)
 
-if response.status_code!= 200:
+if response.status_code != 200:
   print("error")
 
 data = response.json()
 
-print(data)
+#print(data)
+count = data['count']
+results = data['results']
+#print(count)
+
+import random
+random_result_ix = random.randint(0, count-1)
+random_result = results[random_result_ix]
+
+print("here is an alignment:")
+print (random_result["name"])
+
 
 #for key, value in data.items():
-  #print (key, value)
+#print (key, value)
 
 #print (data["count"])
