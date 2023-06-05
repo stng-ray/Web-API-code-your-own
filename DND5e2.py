c1374a6f003a67d7664d5e83e3eeebf616b7162f
HydraulicPressd <123489382+HydraulicPressd@users.noreply.github.com>
2023-06-05 16:15:18 +0000

    slay

---
+++ DND5e2.py
@@ -0,0 +1,35 @@
+import requests
+
+URL = "https://www.dnd5eapi.co/api/"
+
+attribute = input("what attribute would you like?").lower().strip()
+allowed_attributes = ["alignments", "races"]
+while attribute not in allowed_attributes:
+  attribute = input("Choose one of", allowed_attributes, ": ")
+
+NewURL = URL + attribute
+
+response = requests.get(NewURL)
+
+if response.status_code != 200:
+  print("error")
+
+data = response.json()
+
+
+#print(data)
+count = data['count']
+results = data['results']
+#print(count)
+
+import random
+
+random_result_ix = random.randint(0, count - 1)
+random_result = results[random_result_ix]
+
+print(random_result["name"])
+
+#for key, value in data.items():
+#print (key, value)
+
+#print (data["count"])
---
+++ boogers.py
@@ -0,0 +1,42 @@
+#import DND5e
+
+
+import DND5e2
+import requests
+
+URL = "https://www.dnd5eapi.co/api"
+response = requests.get(URL)
+
+if response.status_code != 200:
+  print("error")
+
+#amog
+
+# specify = input("""What would you like randomized? Choices listed below:
+# ability-scores
+# alignments
+# backgrounds
+# classes
+# conditions
+# damage-types
+# equipment
+# equipment-categories
+# feats
+# features
+# languages
+# magic-items
+# magic-schools
+# monsters
+# proficiencies
+# races
+# rule-selections
+# rules
+# skills
+# spells
+# subclasses
+# subraces
+# traits
+# weapon-properties
+# """)
+
+#print(specify)
